5ca6b8d857b1282d403c601d8cb66255f0ceb39f
Main <alexander.swanson.1@uvm.edu>
2018-12-04 13:04:14 -0500

    Uploaded main files.

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * Created by Alexander Swanson on 11/30/18.
+ * Copyright (c) 2018, Alexander Joseph Swanson Villares
+ * alexjosephswanson@gmail.com
+ *
+ * Resource: https://brennan.io/2015/01/16/write-a-shell-in-c/
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
